c1d0c55a7c463273b1109e84187e97ba76fcafcd
Fadhil <fadhil.akins@gmail.com>
2020-04-09 04:36:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,249 @@
+//this is an illustration of a database of quotes from the movie 'V for Vendetta'
+let quotesdb = [{"quote": "People shouldn't be afraid of their government. Governments should be afraid of their people.","quoted_by":"V","origin_title":"V for Vendetta","origin_author":"Alan Moore"},
+    {"quote": "Beneath this mask there is more than flesh, Beneath this mask there is an idea, Mr. Creedy, and ideas are bulletproof.",
+        "quoted_by": "V",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "A revolution without dancing is a revolution not worth having.",
+        "quoted_by": "V",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "There are no coincidences, only the illusion of coincidences",
+        "quoted_by": "V",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "The only verdict is vengeance a vendetta",
+        "quoted_by": "V",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "No one will ever forget that night and what it meant for this country. But I will never forget the man and what he meant to me",
+        "quoted_by": "Evey Hammond",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "We are told to remember the idea, not the man, because a man can fail. He can be caught, he can be killed and forgotten, but 400 years later, an idea can still change the world",
+        "quoted_by": "Evey Hammond",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "Remember, remember, the Fifth of November, the Gunpowder Treason and Plot. I know of no reason why the Gunpowder Treason should ever be forgot... But what of the man? I know his name was Guy Fawkes and I know, in 1605, he attempted to blow up the Houses of Parliament. But who was he really? What was he like? We are told to remember the idea, not the man, because a man can fail. He can be caught, he can be killed and forgotten, but 400 years later, an idea can still change the world. I've witnessed first hand the power of ideas, I've seen people kill in the name of them, and die defending them... but you cannot kiss an idea, cannot touch it, or hold it... ideas do not bleed, they do not feel pain, they do not love... And it is not an idea that I miss, it is a man... A man that made me remember the Fifth of November. A man that I will never forget.",
+        "quoted_by": "Evey Hammond",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "Remember, remember, the Fifth of November, the Gunpowder Treason and Plot. I know of no reason why the Gunpowder Treason should ever be forgot.",
+        "quoted_by": "V",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "There is no certainty, only opportunity.",
+        "quoted_by": "V",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "My mother said I broke her heart...but it was my integrity that was important. Is that so selfish? It sells for so little, but it's all we have left in this place. It is the very last inch of us...but within that inch we are free.","quoted_by":"Valerie","origin_title":"V for Vendetta","origin_author":"Alan Moore"},
+    {"quote": "A building is a symbol, as is the act of destroying it. Symbols are given power by people. Alone, a symbol is meaningless, but with enough people, blowing up a building can change the world.",
+        "quoted_by": "V",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "It is to Madame Justice that I dedicate this concerto, in honor of the holiday that she seems to have taken from these parts, and in recognition of the impostor that stands in her stead.",
+        "quoted_by": "V",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "My father was a writer. You would've liked him. He used to say that artists use lies to tell the truth, while politicians use them to cover the truth up.","quoted_by":"Evey Hammond","origin_title":"V for Vendetta","origin_author":"Alan Moore"},
+    {"quote": "To fail is to invite doubt into everything we believe; everything that we have fought for. Doubt will plunge this country back into chaos and I will not let that happen",
+        "quoted_by": "Adam Sutler",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "What was done to me created me. It's a basic principle of the universe that every action causes an equal and opposing reaction.","quoted_by":"V","origin_title":"V for Vendetta","origin_author":"Alan Moore"},
+    {"quote": "But what I hope most of all is that you understand what I mean when I tell you that, even though I do not know you, and even though I may never meet you, laugh with you, cry with you, or kiss you, I love you. With all my heart, I love you.",
+        "quoted_by": "Valerie",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "You wear a mask for so long, you forget who you were beneath it.",
+        "quoted_by": "Deitrich",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "Good guys win, bad guys lose, and as always England prevails!",
+        "quoted_by": "Lewis Prothero",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "The multiplying villainies of nature do swarm upon him. And fortune, on his damned quarrel, smiling, showed like a rebel's whore. Disdaining fortune with his brandished steel, which smoked with bloody execution","quoted_by":"V","origin_title":"V for Vendetta","origin_author":"Alan Moore"},
+    {"quote": "Happiness is the most insidious prison of all, Evey.",
+        "quoted_by": "V",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "Gordon please, I don't need mister to make this body feel any older.","quoted_by":"Deitrich","origin_title":"V for Vendetta","origin_author":"Alan Moore"},
+    {"quote": "Your powers of observation continue to serve you well.",
+        "quoted_by": "V",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "While the truncheon may be used in lieu of conversation, words will always retain their power. Words offer the means to meaning, and for those who will listen, the enunciation of truth.",
+        "quoted_by": "V",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "The past can't hurt you anymore. Not unless you let it. They made you into a victim, Evey. They made you into a statistic. But, that's not the real you. That's not who you are inside.","quoted_by":"V","origin_title":"V for Vendetta","origin_author":"Alan Moore"},
+    {"quote": "If our own government was responsible for the deaths of almost a hundred thousand people... would you really want to know?",
+        "quoted_by": "Fitch",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "I want this country to realize that we stand on the edge of oblivion. I want everyone to remember why they need us!",
+        "quoted_by": "Adam Sutler",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "We are being buried under the avalanche of your inadequacies, Mr. Creedy!",
+        "quoted_by": "Adam Sutler",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "Defiant to the end, huh? You won't cry like him, will you? You're not afraid of death. You're like me.","quoted_by":"Peter Creedy","origin_title":"V for Vendetta","origin_author":"Alan Moore"},
+    {"quote": "I wish I wasn't afraid all the time, but... I am.","quoted_by":"Evey Hammond","origin_title":"V for Vendetta","origin_author":"Alan Moore"},
+    {"quote": "Evey, please. . .there is a face beneath this mask, but it's not me. I'm no more that face than I am the muscles beneath it, or the bones beneath them.",
+        "quoted_by": "V",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "Words are the means to meaning, and for those who listen, the enunciation of truth, and the truth is there is something terribly wrong with this country, isn't there?","quoted_by":"V","origin_title":"V for Vendetta","origin_author":"Alan Moore"},
+    {"quote": "Creedy: Whatchya gonna do, huh? We've swept this place. You've got nothing. Nothing but your bloody knives and your fancy karate gimmicks. We have guns. \n V: No, what you have are bullets, and the hope that when your guns are empty I'm no longer be standing, because if I am you'll all be dead before you've reloaded.","quoted_by":"Dialogue between Creedy and V","origin_title":"V for Vendetta","origin_author":"Alan Moore"},
+    {"quote": "In view, a humble vaudevillian veteran, cast vicariously as both victim and villain by the vicissitudes of Fate. This visage, no mere veneer of vanity, is a vestige of the vox populi, now vacant, vanished.”",
+        "quoted_by": "V",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "Evey: You just said you killed Lewis Prothero! \n V: I might have killed the fingerman who attacked you, but I heard no objection then. \n Evey: What? \n V: Violence can be used for good.",
+        "quoted_by": "Dialogue between V and Evey",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "Violence can be used for good.",
+        "quoted_by": "V",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "This valorous visitation of a by-gone vexation, stands vivified and has vowed to vanquish these venal and virulent vermin vanguarding vice and vouchsafing the violently vicious and voracious violation of volition.",
+        "quoted_by": "V",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "The truth is, there is something terribly wrong with this country, isn't there? Cruelty and injustice, intolerance and oppression. And where once you had the freedom to object, to think and speak as you saw fit, you now have censors and systems of surveillance coercing your conformity and soliciting your submission.","quoted_by":"V","origin_title":"V for Vendetta","origin_author":"Alan Moore"},
+    {"quote": "I dare do all that may become a man, Who dares do more is none.",
+        "quoted_by": "V",
+        "origin_title": "Macbeth",
+        "origin_author": "Shakespeare"
+    },
+    {"quote": "And thus I clothe my naked villainy With old odd ends stolen forth from holy writ And seem a saint when most I play the devil.",
+        "quoted_by": "V",
+        "origin_title": "Richard III",
+        "origin_author": "Shakespeare"
+    },
+    {"quote": "We are oft to blame in this, 'Tis too much proved - that with devotion's visage, And pious action, we do sugar o'er The devil himself.","quoted_by":"V","origin_title":"Shakespeare's Hamlet Act 3, Scene 1",
+        "origin_author": "Polonius"
+    },
+    {"quote": "One thing is true of all governments - their most reliable records are tax records.",
+        "quoted_by": "Fitch",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "Lewis Prothero: You... it is you! \n V: The Ghost of Christmas past.",
+        "quoted_by": "Dialogue between V and Lewis Prothero",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    },
+    {"quote": "England prevails",
+        "quoted_by": "Adam Sutler",
+        "origin_title": "V for Vendetta",
+        "origin_author": "Alan Moore"
+    }
+]
+
+/*this function receives the quote in the db 
+to check if they are original to the movie 
+or were sampled from other literary masterpieces
+*/
+let quoteIsOriginal = ((data) => {if (data['origin_title'] =='V for Vendetta') {return true} else {return false}
+})
+
+
+//this is a sample of what a backend receiving the request would look like 
+const getRandomQuote = (data) => {
+    //this checks the length of the database
+    const lengthOfDb = data.length - 1;
+
+    // since we would be using random quotes, this generates a random quote object from the db
+    const quoteIndex = Math.floor(Math.random() * (lengthOfDb - 0) + 0);
+
+    //this gets the quote object and stores it in the quoteObj variable
+    const quoteObj = data[quoteIndex];
+
+    //this stores an actual quote
+    const quote = quoteObj['quote'];
+
+    //this initializes the author to ''let quoteAuthor =''
+
+    //this simple if function utilizes the function created to check if the quote is original
+    if (quoteIsOriginal(quoteObj)) {
+
+        //if the quote is original it sets the author to the person who made the quote in the movie
+        quoteAuthor = quoteObj['quoted_by'];
+    } else {
+
+        //if the quote was sampled it sets the author to the original book/movie and the person who said it 
+        quoteAuthor = `V quoting ${quoteObj['origin_author']} from ${quoteObj['origin_title']}`
+    }
+
+
+    //this is the return statement for the object. It returns an object with a quote and the corresponding author when called
+    return {"requestedQuote": quote,
+        "requestedQuoteAuthor": quoteAuthor
+    }
+}
+
+//this creates a variable for the text placeholder where the quote would be displayed
+let quoteplaceholder = document.getElementById('text');
+
+//this creates a variable for the text placeholder where the author of the quote would be displayed
+let authorPlaceholder = document.getElementById('author');
+
+let newQuoteButton = document.getElementById('new-quote');
+
+/*this function is run when the page loads and when there is an initiation by a user
+the purpose is to get a random quote from the source and display it onto the placeholders*/
+const QUOTE = (() => {let randomQuote = getRandomQuote(quotesdb);
+
+
+    //this constructor is used to get the quotes 
+    function VendettaQuote(randomQuote) {this.quote = randomQuote['requestedQuote'];
+
+        this.quoteAuthor = randomQuote['requestedQuoteAuthor'];
+
+
+    }
+
+
+    let vendetta = new VendettaQuote(randomQuote);
+
+    quoteplaceholder.innerHTML = vendetta.quote.replace(/\n/g, "<br><br>");
+    authorPlaceholder.innerHTML = vendetta.quoteAuthor
+
+})();
+
+
+
+
+// let newquote=()=>{// 	getRandomQuote(quotesdb)
+// }
